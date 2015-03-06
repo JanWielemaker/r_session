71bb126961fa3d4bc11906d2f97e60c128cdd891
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-03-06 21:21:29 +0100

    MODIFIED: Repackaging as a Prolog pack

---
+++ pack.pl
@@ -0,0 +1,6 @@
+name(r_session).
+title('Interface to an R process').
+version('1.0.0').
+keywords([r, statistics]).
+author('Nicos Angelopoulos', 'nicos.angelopoulos@gmail.com').
+download('http://www.swi-prolog.org/download/pack/r_session-*.tgz').
