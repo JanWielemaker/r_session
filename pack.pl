b9b381a7393bc53c436c2406cb5464d7251ce316
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-03-06 21:51:17 +0100

    Prepare for publication on github

--- pack.pl
+++ pack.pl
@@ -3,4 +3,4 @@ title('Interface to an R process').
 version('1.0.0').
 keywords([r, statistics]).
 author('Nicos Angelopoulos', 'nicos.angelopoulos@gmail.com').
-download('http://www.swi-prolog.org/download/pack/r_session-*.tgz').
+download('https://github.com/JanWielemaker/r_session/archive/V*.zip').
